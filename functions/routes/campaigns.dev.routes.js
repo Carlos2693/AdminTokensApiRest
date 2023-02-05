1f7696c64bb0f85f42ecf9f2b2e47362d4272a87
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2023-02-04 18:56:33 -0600

    [Improve] Valid token version in get

--- functions/routes/campaigns.dev.routes.js
+++ functions/routes/campaigns.dev.routes.js
@@ -26,62 +26,67 @@ router.get('/apidev/campaigns/:tracker_id/tokens', async(req, res) => {
 
     try {
 
-        const version_tokens = parseInt(req.query.version_tokens)
-        if (Number.isNaN(version_tokens)) {
-            return res.status(200).json({versionToken: 'No version'})
+        var versionTokens = 0
+        var version_tokens = req.query.version_tokens
+        if (version_tokens !== '') {
+            versionTokens = parseInt(req.query.version_tokens)
+            if (Number.isNaN(version_tokens)) {
+                return res.status(200).json({status: false})
+            }
+        }
+
+        const trackerId = req.params.tracker_id
+
+        const queryCampaign = db.collection(campaigns)
+            .doc(trackerId)
+        const snapshotCampaign = await queryCampaign.get()
+        
+        var updateSwaps = false
+        
+        if (snapshotCampaign.exists) {
+            const data = snapshotCampaign.data()
+            versionTokens = data.versionTokens
+
+            updateSwaps = versionTokens > version_tokens
         } else {
+            updateSwaps = false
+        }
 
-            const trackerId = req.params.tracker_id
+        if (updateSwaps) {
 
-            const queryCampaign = db.collection(campaigns)
+            const query = db.collection(campaigns)
                 .doc(trackerId)
-            const snapshotCampaign = await queryCampaign.get()
-            
-            var updateSwaps = false
-            if (snapshotCampaign.exists) {
-                const data = snapshotCampaign.data()
-                const versionTokens = data.versionTokens
+                .collection(tokens)
+                .orderBy('tokenNumber', 'desc')
+            const querySnapshot = await query.get()
+            const docs = querySnapshot.docs
 
-                updateSwaps = versionTokens > version_tokens
-            } else {
-                updateSwaps = false
-            }
+            const array = docs.map(doc => {
 
-            if (updateSwaps) {
+                const data = doc.data()
+                const objetives = data.objetives
+                
+                return {
+                    id: doc.id,
+                    dateToken: data.dateToken,
+                    descriptionToken: data.descriptionToken,
+                    image: data.image,
+                    tokenName: data.tokenName,
+                    tokenNumber: data.tokenNumber,
+                    objetives: objetives !== undefined ? objetives : []
+                }
+            })
 
-                const query = db.collection(campaigns)
-                    .doc(trackerId)
-                    .collection(tokens)
-                    .orderBy('tokenNumber', 'desc')
-                const querySnapshot = await query.get()
-                const docs = querySnapshot.docs
-
-                const array = docs.map(doc => {
-
-                    const data = doc.data()
-                    const objetives = data.objetives
-                    
-                    return {
-                        id: doc.id,
-                        dateToken: data.dateToken,
-                        descriptionToken: data.descriptionToken,
-                        image: data.image,
-                        tokenName: data.tokenName,
-                        tokenNumber: data.tokenNumber,
-                        objetives: objetives !== undefined ? objetives : []
-                    }
-                })
-
-                return res.status(200).json({
-                    status: true,
-                    array: array
-                })
-            } else {
-                return res.status(200).json({
-                    status: false
-                })
-            }
-        }        
+            return res.status(200).json({
+                status: true,
+                versionTokens: versionTokens,
+                array: array
+            })
+        } else {
+            return res.status(200).json({
+                status: false
+            })
+        }
     } catch (error) {
         return res.status(500).send(error)
     }
